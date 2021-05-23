6c48540ff9e6caa74d86eb1715308b1d8e90845b
Undercover Stamp <byemc@outlook.com>
2021-05-23 20:20:03 +0100

    removed MC stuff

--- byetools.py
+++ byetools.py
@@ -1,22 +1,3 @@
-def get_uuid(ign):
-    import json
-    import requests
-    apiresponse = requests.get('https://api.mojang.com/users/profiles/minecraft/{}'.format(ign))
-    data = json.loads(apiresponse.text)
-    return data
-
-def get_hypixel_data(uuid):
-    import json, requests
-    jsn = requests.get('https://api.hypixel.net/player?key=20f1f2a0-c2b7-47c6-8e58-4f01619ca8ac&uuid={}'.format(uuid))    
-    data = jsn.json() # puts it all into a variable that can be managed by the program calling this function
-    return data # gives it to another program
-
-def played_bedwars(uuid):
-    import json, requests
-    jsn = requests.get('https://api.hypixel.net/player?key=20f1f2a0-c2b7-47c6-8e58-4f01619ca8ac&uuid={}'.format(uuid))    
-    data = jsn.json() # puts it all into a variable that can be managed by the program calling this function
-    return data['player']['games_played_bedwars_1'] # gives it to another program
-
 def get_os():
     import sys
     platforms = {'linux' : 'Linux', 'linux1' : 'Linux', 'linux2' : 'Linux', 'darwin' : 'OS X', 'win32' : 'Windows'}
